48755b9d23bcdb552210e71235cf31d6b66bb33f
tangthiendat <thiendattang@gmail.com>
2024-05-27 19:25:17 +0700

    Define component props types

---
+++ 01-react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,11 @@
+export default function CourseGoal({ title, description }: { title: string; description: string }) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
