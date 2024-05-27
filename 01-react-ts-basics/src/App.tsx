fc88a608bde087228a062f26a966261800b4961a
tangthiendat <thiendattang@gmail.com>
2024-05-27 20:36:00 +0700

    Work with State and Statebased Values

--- 01-react-ts-basics/src/App.tsx
+++ 01-react-ts-basics/src/App.tsx
@@ -12,7 +12,16 @@ interface CourseGoal {
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-  function handleAddGoal() {}
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
 
   return (
     <main>
@@ -20,9 +29,15 @@ export default function App() {
         <p>Your Course Goals</p>
       </Header>
       <button onClick={handleAddGoal}>Add Goals</button>
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
