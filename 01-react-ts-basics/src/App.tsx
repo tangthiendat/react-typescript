788161de607c6764ccdba773355f7a14aea8cf67
tangthiendat <thiendattang@gmail.com>
2024-05-27 20:14:55 +0700

    Create a Header Component

--- 01-react-ts-basics/src/App.tsx
+++ 01-react-ts-basics/src/App.tsx
@@ -1,8 +1,13 @@
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <p>Your Course Goals</p>
+      </Header>
       <CourseGoal title="Learn React + TS">
         <p>Learn it from the ground up</p>
       </CourseGoal>
---
+++ 01-react-ts-basics/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+
+type HeaderProps = PropsWithChildren<{
+  image: {
+    src: string;
+    alt: string;
+  };
+}>;
+
+const Header: React.FC<HeaderProps> = ({ image, children }) => {
+  return (
+    <header>
+      <img src={image.src} alt={image.alt} />
+      {children}
+    </header>
+  );
+};
+
+export default Header;
