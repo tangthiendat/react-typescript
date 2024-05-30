299c57b0e5e250218be10a0141649fe083658f27
tangthiendat <thiendattang@gmail.com>
2024-05-30 15:10:46 +0700

    Fetch transforming data

--- 06-data-fetching/src/App.tsx
+++ 06-data-fetching/src/App.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import fetchingImg from "./assets/data-fetching.png";
+
+interface RawBlogPost {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
 const App: React.FC = () => {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPost, setFetchedPost] = useState<BlogPost[] | undefined>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data: RawBlogPost[] = await get<RawBlogPost[]>(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+      setFetchedPost(blogPosts);
+    }
+
+    fetchPosts();
+  });
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image depicting a data fetching process" />
+      {fetchedPost && <BlogPosts posts={fetchedPost} />}
+    </main>
+  );
 };
 
 export default App;
--- 06-data-fetching/src/util/http.ts
+++ 06-data-fetching/src/util/http.ts
@@ -1,8 +1,8 @@
-export async function get(url: string): Promise<unknown> {
+export async function get<T>(url: string): Promise<T> {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
   const data = (await response.json()) as unknown;
-  return data;
+  return data as T;
 }
