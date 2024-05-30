3f11100c690a922920dcdac19308f29f0ff7a3ec
tangthiendat <thiendattang@gmail.com>
2024-05-30 15:37:05 +0700

    Handle loading error

--- 06-data-fetching/src/App.tsx
+++ 06-data-fetching/src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 import BlogPosts, { type BlogPost } from "./components/BlogPosts";
 import { get } from "./util/http";
 import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 interface RawBlogPost {
   id: number;
@@ -11,31 +12,44 @@ interface RawBlogPost {
 }
 
 const App: React.FC = () => {
-  const [fetchedPost, setFetchedPost] = useState<BlogPost[] | undefined>();
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data: RawBlogPost[] = await get<RawBlogPost[]>(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      try {
+        setIsFetching(true);
+        const data: RawBlogPost[] = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawBlogPost[];
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
-      setFetchedPost(blogPosts);
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+      setIsFetching(false);
     }
 
     fetchPosts();
-  });
+  }, []);
 
   return (
     <main>
       <img src={fetchingImg} alt="An abstract image depicting a data fetching process" />
-      {fetchedPost && <BlogPosts posts={fetchedPost} />}
+      {isFetching && <p id="loading-fallback">Fetching posts...</p>}
+      {fetchedPosts && <BlogPosts posts={fetchedPosts} />}
+      {error && <ErrorMessage text={error} />}
     </main>
   );
 };
--- 06-data-fetching/src/util/http.ts
+++ 06-data-fetching/src/util/http.ts
@@ -1,8 +1,8 @@
-export async function get<T>(url: string): Promise<T> {
+export async function get(url: string): Promise<unknown> {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
   const data = (await response.json()) as unknown;
-  return data as T;
+  return data;
 }
