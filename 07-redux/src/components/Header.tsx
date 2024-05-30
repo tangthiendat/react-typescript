e41e3b8a07b7bb174019e9af0845e6e032634323
tangthiendat <thiendattang@gmail.com>
2024-05-30 19:25:21 +0700

    Create a TypeSafe useSelector hook

--- 07-redux/src/components/Header.tsx
+++ 07-redux/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 import Cart from "./Cart.tsx";
+import { useCartSelector } from "../store/hook.ts";
 
 const Header: React.FC = () => {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const cartQuantity = useCartSelector((state) =>
+    state.cart.items.reduce((acc, item) => acc + item.quantity, 0)
+  );
 
   function handleOpenCartClick() {
     setCartIsVisible(true);
@@ -22,7 +26,7 @@ const Header: React.FC = () => {
           <h1>Elegant Redux</h1>
         </div>
         <p>
-          <button onClick={handleOpenCartClick}>Cart (0)</button>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
         </p>
       </header>
     </>
