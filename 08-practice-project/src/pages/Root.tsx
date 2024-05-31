df30418f752665b83e52315084ed1ba8f5756bec
tangthiendat <thiendattang@gmail.com>
2024-05-31 08:52:34 +0700

    Init practice project

---
+++ 08-practice-project/src/pages/Root.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Root: React.FC = () => {
+  return (
+    <>
+      {/* Todo: Add Header */}
+      <Outlet />
+    </>
+  );
+};
+
+export default Root;
